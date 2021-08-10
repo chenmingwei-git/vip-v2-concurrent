e35b85ededaf3030b81e9be9b53b60d2ecb37b7f
陈明伟 <chenmingwei@qinjia-tech.com>
2021-08-10 16:47:38 +0800

    格式化

--- concurrent/src/main/java/cn/enjoyedu/ch6/ThreadPoolAdv.java
+++ concurrent/src/main/java/cn/enjoyedu/ch6/ThreadPoolAdv.java
@@ -7,15 +7,14 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- *类说明：自定义线程池中线程的创建方式，把线程设置为守护线程
+ * 类说明：自定义线程池中线程的创建方式，把线程设置为守护线程
  */
 public class ThreadPoolAdv {
-    static class Worker implements Runnable
-    {
+    static class Worker implements Runnable {
         private String taskName;
         private Random r = new Random();
 
-        public Worker(String taskName){
+        public Worker(String taskName) {
             this.taskName = taskName;
         }
 
@@ -24,36 +23,34 @@ public class ThreadPoolAdv {
         }
 
         @Override
-        public void run(){
+        public void run() {
             System.out.println(Thread.currentThread().getName()
-            		+" process the task : " + taskName);
-            SleepTools.ms(r.nextInt(100)*5);
+                    + " process the task : " + taskName);
+            SleepTools.ms(r.nextInt(100) * 5);
         }
     }
 
-    private static class MyThreadFactory implements ThreadFactory{
+    private static class MyThreadFactory implements ThreadFactory {
 
         private AtomicInteger count = new AtomicInteger(0);
 
         @Override
         public Thread newThread(Runnable r) {
-            Thread t = new Thread(r,"Mark_"+count.getAndIncrement());
+            Thread t = new Thread(r, "Mark_" + count.getAndIncrement());
             t.setDaemon(true);
-            System.out.println("create "+t);
+            System.out.println("create " + t);
             return t;
         }
     }
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException
-    {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService threadPool = new ThreadPoolExecutor(2,
                 4, 3,
                 TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(10),
                 new MyThreadFactory(),
                 new ThreadPoolExecutor.DiscardOldestPolicy());
 
-        for (int i = 0; i <= 6; i++)
-        {
+        for (int i = 0; i <= 6; i++) {
             Worker worker = new Worker("worker " + i);
             System.out.println("A new task has been added : " + worker.getName());
             threadPool.execute(worker);
